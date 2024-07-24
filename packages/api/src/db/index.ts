92f9f0360a3c5486b02b0d9bbe70dcf5c9321cee
Anderson Fernandes <fernandesanderson14@gmail.com>
2024-07-24 11:04:54 -0300

    Add zod env management
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- packages/api/src/db/index.ts
+++ packages/api/src/db/index.ts
@@ -1,8 +1,9 @@
+import { env } from "@/env";
 import knex from "knex";
 
 export const db = knex({
-  client: "postgres",
-  connection: process.env.DATABASE_URL,
+  client: "pg",
+  connection: env.DATABASE_URL,
   pool: {
     min: 2,
     max: 20,
---
+++ packages/api/src/env.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+const schema = z.object({
+  DATABASE_URL: z
+    .string()
+    .url()
+    .default("postgres://postgres:postgres@localhost:5432/house_watch"),
+});
+
+const setupEnv = () => {
+  const { data, error } = schema.safeParse(process.env);
+
+  if (error) {
+    const errorMessages = Object.entries(error.flatten().fieldErrors).map(
+      ([key, value]) => `- ${key}:\t${value}`
+    );
+    throw new Error(`Invalid environment:\n${errorMessages.join("\n")}`);
+  }
+
+  return data;
+};
+
+export const env = setupEnv();
